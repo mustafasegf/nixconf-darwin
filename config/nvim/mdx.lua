78a7dbf9206fb3d58b1fa3091b2120f0114a9c40
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2026-01-31 23:47:35 +0700

    fix: add autocmd to ensure treesitter attaches to MDX files

--- config/nvim/mdx.lua
+++ config/nvim/mdx.lua
@@ -3,3 +3,11 @@
 -- This enables proper syntax highlighting for MDX files including code blocks
 
 vim.treesitter.language.register('markdown', 'mdx')
+
+-- Ensure treesitter highlighting is attached to MDX buffers
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "mdx",
+  callback = function()
+    vim.treesitter.start()
+  end,
+})

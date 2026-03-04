37545ada20bda1330f954fc7ba97bb451796d6a3
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2026-03-05 01:44:04 +0700

    refactor(ghostty): remove macOS nix overlay, keep flake for linux only

--- modules/common/gui.nix
+++ modules/common/gui.nix
@@ -1,17 +1,24 @@
 {
   pkgs,
+  lib,
+  inputs,
   ...
 }:
 
 {
-  environment.systemPackages = with pkgs; [
-    iterm2
-    kitty
-    postman
-    dbeaver-bin
-    slack
-    discord
-    wireshark
-    handy
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      iterm2
+      kitty
+      postman
+      dbeaver-bin
+      slack
+      discord
+      wireshark
+      handy
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
 }

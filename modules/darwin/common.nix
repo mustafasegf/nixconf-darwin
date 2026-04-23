94265ded0d2cdf43e8571c407b24eb64580a6de3
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2026-04-23 10:22:40 +0900

    feat(darwin): disable nix daemon and add casks
    Adds bitwarden, openvpn-connect, and scroll-reverser casks to the
    shared darwin config, and disables nix-darwin's nix management in
    favor of Determinate.

--- modules/darwin/common.nix
+++ modules/darwin/common.nix
@@ -4,6 +4,7 @@
   security.pam.services.sudo_local.touchIdAuth = true;
 
   programs.nix-index-database.comma.enable = true;
+  nix.enable = false;
 
   environment.systemPackages = with pkgs; [
     awscli2
@@ -24,6 +25,9 @@
       "mullvad-vpn"
       "vlc"
       "mpv"
+      "bitwarden"
+      "openvpn-connect"
+      "scroll-reverser"
     ];
   };
 }
